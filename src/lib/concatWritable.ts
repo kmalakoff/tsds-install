a265f4446e4f48a335f08a7deabf2d020f7eeb74
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-20 12:41:01 -0800

    Simplified compat layer

--- src/lib/concatWritable.ts
+++ src/lib/concatWritable.ts
@@ -1,12 +1,4 @@
-import Stream from 'stream';
-
-const major = +process.versions.node.split('.')[0];
-let Writable: typeof Stream.Writable;
-if (major > 0) {
-  Writable = Stream.Writable;
-} else {
-  Writable = require('readable-stream').Writable;
-}
+import { Writable } from '../compat.ts';
 
 export type Callback = (lines: Buffer) => void;
 
